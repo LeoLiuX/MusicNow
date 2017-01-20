3c48b1ad0b70e152f0b3fcebd7988e04c76ed3b2
Xiao Liu <liuxiao232@gmail.com>
2017-01-20 14:34:51 -0600

    Structure

--- app/src/main/java/com/example/xiao/musicnow/LoginPage/LoginActivity.java
+++ app/src/main/java/com/example/xiao/musicnow/LoginPage/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.xiao.musicnow.LoginPage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.xiao.musicnow.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
