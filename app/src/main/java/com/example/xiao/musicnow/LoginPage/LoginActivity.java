3f9df1fd274a3d0bd064080b9952fedf7974b39c
Ricky0611 <ruiqixu@163.com>
2017-01-22 03:35:42 -0600

    LoginPage

--- app/src/main/java/com/example/xiao/musicnow/LoginPage/LoginActivity.java
+++ app/src/main/java/com/example/xiao/musicnow/LoginPage/LoginActivity.java
@@ -42,8 +42,6 @@ public class LoginActivity extends AppCompatActivity implements TabLayout.OnTabS
         //Adding adapter to pager
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        //Adding onTabSelectedListener to swipe views
-        tabLayout.setOnTabSelectedListener(this);
     }
 
     @Override
