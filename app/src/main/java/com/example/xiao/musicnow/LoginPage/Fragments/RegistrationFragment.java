e53c83ed5d9f2363b27f51a856b501220ad2d6b4
Ricky0611 <ruiqixu@163.com>
2017-01-22 03:30:55 -0600

    LoginPage

--- app/src/main/java/com/example/xiao/musicnow/LoginPage/Fragments/RegistrationFragment.java
+++ app/src/main/java/com/example/xiao/musicnow/LoginPage/Fragments/RegistrationFragment.java
@@ -1,8 +1,138 @@
 package com.example.xiao.musicnow.LoginPage.Fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.example.xiao.musicnow.Controller.AppController;
+import com.example.xiao.musicnow.LoginPage.PasswordValidator;
+import com.example.xiao.musicnow.R;
+
 /**
  * Created by liuxi on 2017/1/20.
  */
 
-public class RegistrationFragment {
+public class RegistrationFragment extends Fragment{
+    private String TAG = "Registration";
+    TextInputEditText UserName, UserEmail, UserMobile, UserPassword;
+    Button regBtn;
+    String name, email, mobile, password;
+    PasswordValidator validator;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        validator = new PasswordValidator();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_registration, container,false);
+
+        UserName = (TextInputEditText) v.findViewById(R.id.userName);
+        UserEmail = (TextInputEditText) v.findViewById(R.id.userEmail);
+        UserMobile = (TextInputEditText) v.findViewById(R.id.userMobile);
+        UserPassword = (TextInputEditText) v.findViewById(R.id.userPassword);
+        regBtn = (Button) v.findViewById(R.id.regBtn);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = UserName.getText().toString();
+                email = UserEmail.getText().toString();
+                mobile = UserMobile.getText().toString();
+                password = UserPassword.getText().toString();
+                if(!name.isEmpty() && !email.isEmpty() && !mobile.isEmpty() && !password.isEmpty())
+                {
+                    if(validator.validate(password))
+                    {
+                        register(name, email, mobile, password);
+                    }
+                    else
+                    {
+                        UserPassword.setError(validator.PASSWORD_DISCRIPTION);
+                    }
+                }
+                else
+                {
+                    if(name.isEmpty())
+                    {
+                        UserName.setError("Please enter the user name");
+                    }
+                    if(email.isEmpty())
+                    {
+                        UserEmail.setError("Please enter the email address");
+                    }
+                    if(mobile.isEmpty())
+                    {
+                        UserMobile.setError("Please enter the phone number");
+                    }
+                    if(password.isEmpty())
+                    {
+                        UserPassword.setError("Please enter the password");
+                    }
+                }
+            }
+        });
+
+        return v;
+    }
+
+    private void register(String name, String email, String mobile, String password) {
+        String urlReg = "http://rjtmobile.com/ansari/rjt_music/music_app/registration.php?&user_name="+name
+                +"&user_email="+email+"&user_mobile="+mobile+"&user_pass="+password;
+        StringRequest req = new StringRequest(Request.Method.GET, urlReg, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response);
+                if(response.equals("successfully registered"))
+                {
+                    // go back to Login Fragment
+                    LoginFragment fragment = new LoginFragment();
+                    FragmentManager fm=getFragmentManager();
+                    FragmentTransaction ft=fm.beginTransaction();
+                    ft.replace(R.id.pager,fragment).commit();
+                }
+                else
+                {
+                    // mobile number already exist
+                    AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                    alertDialog.setTitle("Attention");
+                    alertDialog.setMessage("Mobile number already exist!");
+                    alertDialog.setCancelable(true);
+                    alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which){
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog ad=alertDialog.create();
+                    ad.show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getActivity(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        AppController.getInstance().addToRequestQueue(req, TAG);
+    }
 }
--- app/src/main/java/com/example/xiao/musicnow/LoginPage/LoginActivity.java
+++ app/src/main/java/com/example/xiao/musicnow/LoginPage/LoginActivity.java
@@ -1,15 +1,63 @@
 package com.example.xiao.musicnow.LoginPage;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.xiao.musicnow.LoginPage.Fragments.LoginFragment;
 import com.example.xiao.musicnow.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("Sign In"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(this);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
